87ea5cc72242766fa43380428bd3252859ad1b00
Juhasz Sandor <blackghost1987@gmail.com>
2022-02-13 18:45:02 +0100

    optional used deposit for refund

--- near-contract-standards/src/ntnft/utils.rs
+++ near-contract-standards/src/ntnft/utils.rs
@@ -1,31 +1,23 @@
 use near_sdk::{env, require, AccountId, Balance, CryptoHash, Promise};
-use std::mem::size_of;
 
-// TODO: need a way for end users to determine how much an approval will cost.
-pub fn bytes_for_approved_account_id(account_id: &AccountId) -> u64 {
-    // The extra 4 bytes are coming from Borsh serialization to store the length of the string.
-    account_id.as_str().len() as u64 + 4 + size_of::<u64>() as u64
-}
-
-
-pub fn refund_deposit_to_account(storage_used: u64, account_id: AccountId) {
+pub fn refund_deposit_to_account(storage_used: u64, account_id: AccountId, deposit_used: Option<Balance>) {
     let required_cost = env::storage_byte_cost() * Balance::from(storage_used);
-    let attached_deposit = env::attached_deposit();
+    let deposite_left = env::attached_deposit() - deposit_used.unwrap_or(0);
 
     require!(
-        required_cost <= attached_deposit,
+        required_cost <= deposite_left,
         format!("Must attach {} yoctoNEAR to cover storage", required_cost)
     );
 
-    let refund = attached_deposit - required_cost;
+    let refund = deposite_left - required_cost;
     if refund > 1 {
         Promise::new(account_id).transfer(refund);
     }
 }
 
-/// Assumes that the precedecessor will be refunded
-pub fn refund_deposit(storage_used: u64) {
-    refund_deposit_to_account(storage_used, env::predecessor_account_id())
+/// Assumes that the predecessor will be refunded
+pub fn refund_deposit(storage_used: u64, deposit_used: Option<Balance>) {
+    refund_deposit_to_account(storage_used, env::predecessor_account_id(), deposit_used)
 }
 
 pub fn hash_account_id(account_id: &AccountId) -> CryptoHash {
